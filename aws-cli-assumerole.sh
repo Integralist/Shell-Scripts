6b056d10f3e425ed9b7e45e4aa78c81d52ceab8f
Integralist <mark.mcdx@gmail.com>
2015-07-29 10:21:29 +0100

    AWS CLI Assume Role

---
+++ aws-cli-assumerole.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+#
+# Dependencies:
+#   brew install jq
+
+unset AWS_SESSION_TOKEN
+export AWS_ACCESS_KEY_ID=<user_access_key>
+export AWS_SECRET_ACCESS_KEY=<user_secret_key>
+
+temp_role=$(aws sts assume-role \
+                    --role-arn "arn:aws:iam::<aws_account_number>:role/<role_name>" \
+                    --role-session-name "<some_session_name>")
+
+export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId)
+export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey)
+export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken)
+
+env | grep -i AWS_
